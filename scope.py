841d48fdac6b40b812acc592efac6eadeb9943cd
viemo777 <viemo777@gmail.com>
2022-11-01 09:48:39 +0200

    Initial commit

---
+++ scope.py
@@ -0,0 +1,48 @@
+import sys
+
+print(sys.path)
+print(sys.api_version)
+print(sys.getwindowsversion())
+print(sys.int_info)
+print(sys.platform)
+print(sys.version_info)
+print(sys.api_version)
+
+pi = 'outer pi variable'
+
+
+def print_pi():
+    pi = 'inner pi variable'
+    print(pi)
+
+
+print_pi()
+print(pi)
+
+pi = 'global pi variable'
+
+
+def inner():
+    pi = 'inner pi variable'
+    print(pi)
+
+
+inner()
+print(pi)
+
+pi = 'global pi variable'
+
+
+def outer():
+    pi = 'outer pi variable'
+
+    def inner():
+        # pi = 'inner pi variable'
+        nonlocal pi
+        print(pi)
+
+    inner()
+
+
+outer()
+print(pi)
